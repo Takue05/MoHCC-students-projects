a7e7a2dc1311d0d1e4ad1bc045e981f826d9fd72
Takue05 <nhematk@gmail.com>
2024-05-15 13:13:10 +0200

    adding the backend boilerplate

---
+++ Backend/src/test/java/com/Students/demo/StudentsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Students.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
